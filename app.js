aacd48306fec01f6f28f6b8c4d6148b11d580cb0
fborlido <fborlido2@gmail.com>
2020-07-09 23:06:50 +0100

    fix register

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db')
 var exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -15,16 +16,15 @@ connectDB();
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}));
+
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-// Body Parser
-app.use(express.urlencoded({extended: false}));
-
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,35 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register Post
+router.post('/register', (req, res) => {
+    const { first_name, last_name, email, password, confirm_password } = req.body;
+    let errors = [];
+
+    // check fields
+    if (!first_name || !last_name || !email || !password || !confirm_password) {
+        errors.push({ msg: 'Please fill in all fields.' })
+    }
+
+    // check passwords
+    if (password !== confirm_password) {
+        errors.push({ msg: 'Passwords don\'t match.' })
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            first_name,
+            last_name,
+            email,
+            password,
+            confirm_password
+        })
+    } else {
+        res.send('correct');
+    }
+
+
+})
+
 module.exports = router;
